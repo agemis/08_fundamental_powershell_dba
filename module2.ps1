c53fc54c46c920e18a5d953f67d335e2cbdb5813
agemis <antoine.gemis@gmail.com>
2023-02-22 13:57:16 +0100

    Examples of pipelines

--- module2.ps1
+++ module2.ps1
@@ -35,3 +35,27 @@ Get-Date -Year 2020 # a little weird here, the current data, but in 2020
 
 # Writing things to the screen
 Write-Host "Hello, here's a message"
+
+
+# Demo 2 - The pipeline
+# Let's see what happends when we pipe the Get-Prcoess cmdlet to another cmdlet that does something with the output
+Get-Process
+Get-Process | Out-File -FilePath c:\lab\pyprocess.txt
+#             note the the parameters are not coherent here between Out-File and Export-CSV
+Get-Process | Export-CSV -Path c:\lab\pyprocess.txt 
+Get-Process | Out-GridView
+
+
+# Select-Object is a great cmdlet to pipe things to, if you want a subset of things returned
+Get-ChildItem | Select-Object 
+Get-ChildItem | Select-Object Name
+Get-ChildItem | Select-Object LastWriteTime
+Get-ChildItem | Select-Object Name, LastWriteTime
+
+# Double pipe !
+Get-ChildItem | Select-Object Name, LastWriteTime | Out-File c:\lab\superpipe.txt
+
+# You can also use parenthesis with cmdlet!
+Get-ChildItem -Path (Resolve-Path ~)
+
+
