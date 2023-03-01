17cbd0dbff3e0403ce4faf0f5cf4d7974234875d
agemis <antoine.gemis@gmail.com>
2023-03-01 21:13:46 +0100

    add module5

---
+++ module5.ps1
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+# LAB 5 ASSIGNMENT
+
+<# Write a function that accepts no parameters and gives you the latest file creation from the path `C:\JunkFiles`
+
+Write another function that accepts a number parameter and returns the number times 1000
+* Bonus: can you include some logic tom male sure the person does not passe zero?
+* Double bonus: Can you make it so you can call this function from a script file?
+
+translation to french: 
+cricket = criquet
+chirps = gazouillis
+
+Write a function that recreates Dolbear's law:
+* Dolbear law says that you can derive temperature from a cricket's chirps using the following equation: `temperature (F) = 50 + (Number of CHripts - 40) / 4`
+* Write a function that accepts the number of chirps and returns the temperature in degress Fahrenheit
+* Bonus: Add a parameter for cricket type! The equation changes on type: 
+	* Field cricket: `F = 50 + ((N - 40) / 4)`
+	* Snowy tree cricket: `F = 50 + ((N - 92) / 4.7)`
+	* Common Katydid: `F = 60 + ((N - 19) / 3)`
+	* Hint: Try the ValidateSet for the cricket type parameter
+	* Double bonus: Make the number of chrips mandatory, but default to the field cricket type
+	* Triple bonus! Can you male it so you can run this function from a file ? 
+ #>
+
+
+ 
